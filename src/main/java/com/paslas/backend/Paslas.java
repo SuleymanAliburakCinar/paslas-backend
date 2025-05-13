171a4b10d1cb35184a7434fd2afab575dbca16c0
Suley <sabc4129@gmail.com>
2025-05-13 18:35:07 +0300

    Initilazing paslas-backend spring project

---
+++ src/main/java/com/paslas/backend/Paslas.java
@@ -0,0 +1,13 @@
+package com.paslas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paslas {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paslas.class, args);
+	}
+
+}
---
+++ src/test/java/com/paslas/backend/PaslasTests.java
@@ -0,0 +1,13 @@
+package com.paslas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaslasTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

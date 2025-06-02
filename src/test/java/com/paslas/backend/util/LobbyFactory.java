315ae6a9d2cca24e9ca22224f96a1a950adf227a
Suley <sabc4129@gmail.com>
2025-06-02 19:17:50 +0300

    1

---
+++ src/test/java/com/paslas/backend/util/LobbyFactory.java
@@ -0,0 +1,10 @@
+package com.paslas.backend.util;
+
+import com.paslas.backend.entity.Lobby;
+
+public class LobbyFactory {
+
+    public static Lobby createLobby(){
+        return new Lobby();
+    }
+}
